46838e6380d4b2da3cf1b8444c00fa0caed1e6c6
chaos0103 <lyt1228@naver.com>
2023-06-07 14:44:49 +0900

    init: setup discover korea project

---
+++ src/main/java/com/ssafy/discoverkorea/DiscoverkoreaApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.discoverkorea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoverkoreaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoverkoreaApplication.class, args);
+	}
+
+}
