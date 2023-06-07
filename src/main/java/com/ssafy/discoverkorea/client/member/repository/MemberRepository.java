7582f5f307980741eb3ca6125ae05662838c26ac
chaos0103 <lyt1228@naver.com>
2023-06-08 01:37:16 +0900

    feat(member): DK-6 회원가입 중복 조회용 쿼리 구현

--- src/main/java/com/ssafy/discoverkorea/client/member/repository/MemberRepository.java
+++ src/main/java/com/ssafy/discoverkorea/client/member/repository/MemberRepository.java
@@ -2,6 +2,22 @@ package com.ssafy.discoverkorea.client.member.repository;
 
 import com.ssafy.discoverkorea.client.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    @Query("select m.id from Member m where m.loginId=:loginId")
+    Optional<Long> existLoginId(@Param("loginId") String loginId);
+
+    @Query("select m.id from Member m where m.tel=:tel")
+    Optional<Long> existTel(@Param("tel") String tel);
+
+    @Query("select m.id from Member m where m.email=:email")
+    Optional<Long> existEmail(@Param("email") String email);
+
+    @Query("select m.id from Member m where m.nickname=:nickname")
+    Optional<Long> existNickname(@Param("nickname") String nickname);
 }
