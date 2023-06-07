5c3f598f1a46b22ce799aec9b5c337c737d5bd05
chaos0103 <lyt1228@naver.com>
2023-06-07 16:27:20 +0900

    feat(member): DK-6 Member 엔티티에 시큐리티 권한 적용

--- src/main/java/com/ssafy/discoverkorea/client/member/Member.java
+++ src/main/java/com/ssafy/discoverkorea/client/member/Member.java
@@ -5,14 +5,23 @@ import com.ssafy.discoverkorea.common.entity.TimeBaseEntity;
 import com.ssafy.discoverkorea.common.entity.UploadFile;
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static javax.persistence.EnumType.*;
+import static javax.persistence.FetchType.*;
 
 @Entity
 @Getter
-public class Member extends TimeBaseEntity {
+public class Member extends TimeBaseEntity implements UserDetails {
 
     @Id @GeneratedValue
     @Column(name = "member_id")
@@ -40,12 +49,15 @@ public class Member extends TimeBaseEntity {
     @Column(nullable = false, length = 20)
     private Active active;
 
+    @ElementCollection(fetch = EAGER)
+    private List<String> roles = new ArrayList<>();
+
     //== 생성자 ==//
     public Member() {
     }
 
     @Builder
-    public Member(Long id, String loginId, String loginPw, String name, String tel, String email, String birth, Gender gender, String nickname, UploadFile uploadFile, Active active) {
+    public Member(Long id, String loginId, String loginPw, String name, String tel, String email, String birth, Gender gender, String nickname, UploadFile uploadFile, Active active, List<String> roles) {
         this.id = id;
         this.loginId = loginId;
         this.loginPw = loginPw;
@@ -57,5 +69,44 @@ public class Member extends TimeBaseEntity {
         this.nickname = nickname;
         this.uploadFile = uploadFile;
         this.active = active;
+        this.roles = roles;
+    }
+
+    //== 시큐리티 설정 ==//
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return loginId;
+    }
+
+    @Override
+    public String getPassword() {
+        return loginPw;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
