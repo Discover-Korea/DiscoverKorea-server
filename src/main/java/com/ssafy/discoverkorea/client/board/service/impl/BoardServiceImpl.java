569777f9cba57faabe631ae8871d6297f2ab009b
chaos0103 <lyt1228@naver.com>
2023-06-08 23:32:42 +0900

    feat(board): DK-14 게시글 삭제 서비스 구현

--- src/main/java/com/ssafy/discoverkorea/client/board/service/impl/BoardServiceImpl.java
+++ src/main/java/com/ssafy/discoverkorea/client/board/service/impl/BoardServiceImpl.java
@@ -43,7 +43,11 @@ public class BoardServiceImpl implements BoardService {
 
     @Override
     public Long removeBoard(Long boardId) {
-        return null;
+        Board findBoard = boardRepository.findById(boardId)
+                .orElseThrow(NoSuchElementException::new);
+
+        findBoard.removeBoard();
+        return findBoard.getId();
     }
 
     private Board toBoard(AddBoardDto dto, Member member) {
