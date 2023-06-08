7dc7938bef2fb7c2d57c759e77dda752e53ad651
chaos0103 <lyt1228@naver.com>
2023-06-08 15:33:51 +0900

    feat(member): DK-8 회원 비밀번호 수정 api 컨트롤러 구현

---
+++ src/main/java/com/ssafy/discoverkorea/client/api/MemberApiController.java
@@ -0,0 +1,41 @@
+package com.ssafy.discoverkorea.client.api;
+
+import com.ssafy.discoverkorea.client.api.request.member.EditLoginPwRequest;
+import com.ssafy.discoverkorea.client.member.service.MemberService;
+import com.ssafy.discoverkorea.client.member.service.dto.EditLoginPwDto;
+import com.ssafy.discoverkorea.jwt.SecurityUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/my")
+@Api(tags = {"마이페이지"})
+public class MemberApiController {
+
+    private final MemberService memberService;
+
+    @ApiOperation(value = "비밀번호 변경")
+    @PutMapping("/loginPw")
+    public void editLoginPw(@Valid @RequestBody EditLoginPwRequest request) {
+        log.debug("EditLoginPwRequest={}", request);
+        String loginId = SecurityUtil.getCurrentLoginId();
+        log.debug("loginId={}", loginId);
+
+        EditLoginPwDto dto = EditLoginPwDto.builder()
+                .nowLoginPw(request.getNowLoginPw())
+                .newLoginPw(request.getNewLoginPw())
+                .build();
+        Long memberId = memberService.editLoginPw(loginId, dto);
+        log.debug("editLoginPw member={}", memberId);
+    }
+}
