56c324efada5645947ec155df7d6df465a13fc82
chaos0103 <lyt1228@naver.com>
2023-06-08 23:11:59 +0900

    feat(board): DK-13 게시글 수정 서비스 골격 생성

--- src/main/java/com/ssafy/discoverkorea/client/board/service/BoardService.java
+++ src/main/java/com/ssafy/discoverkorea/client/board/service/BoardService.java
@@ -1,10 +1,13 @@
 package com.ssafy.discoverkorea.client.board.service;
 
 import com.ssafy.discoverkorea.client.board.service.dto.AddBoardDto;
+import com.ssafy.discoverkorea.client.board.service.dto.EditBoardDto;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
 public interface BoardService {
 
     Long addBoard(String loginId, AddBoardDto dto);
+
+    Long editBoard(Long boardId, EditBoardDto dto);
 }
