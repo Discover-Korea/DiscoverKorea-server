fad9093b1417fc0b25bd2d5ef5fcef45942ffa2d
chaos0103 <lyt1228@naver.com>
2023-06-09 12:32:11 +0900

    feat(board): DK-19 BoardScrapRepository 생성 및 DataJPA 적용

---
+++ src/main/java/com/ssafy/discoverkorea/client/board/repository/BoardScrapRepository.java
@@ -0,0 +1,7 @@
+package com.ssafy.discoverkorea.client.board.repository;
+
+import com.ssafy.discoverkorea.client.board.BoardScrap;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardScrapRepository extends JpaRepository<BoardScrap, Long> {
+}
