ec341b30d3eb475c4b741fca60e2687fc26f3fe1
chaos0103 <lyt1228@naver.com>
2023-06-12 13:12:40 +0900

    style(board): DK-21 미사용 import 제거

--- src/test/java/com/ssafy/discoverkorea/client/board/BoardCommentTest.java
+++ src/test/java/com/ssafy/discoverkorea/client/board/BoardCommentTest.java
@@ -1,12 +1,11 @@
 package com.ssafy.discoverkorea.client.board;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.ssafy.discoverkorea.common.entity.Active.ACTIVE;
 import static com.ssafy.discoverkorea.common.entity.Active.DEACTIVE;
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class BoardCommentTest {
 
