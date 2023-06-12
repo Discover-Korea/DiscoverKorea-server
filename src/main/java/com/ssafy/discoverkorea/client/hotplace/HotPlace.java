00037ea291259a9c6b476cf3c0ba8e02f6a1dab8
chaos0103 <lyt1228@naver.com>
2023-06-12 16:11:14 +0900

    feat(hotPlace): DK-23 HotPlace 도메인 설계

---
+++ src/main/java/com/ssafy/discoverkorea/client/hotplace/HotPlace.java
@@ -0,0 +1,53 @@
+package com.ssafy.discoverkorea.client.hotplace;
+
+import com.ssafy.discoverkorea.client.member.Member;
+import com.ssafy.discoverkorea.common.entity.Active;
+import com.ssafy.discoverkorea.common.entity.TimeBaseEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+import static javax.persistence.EnumType.*;
+import static javax.persistence.FetchType.*;
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+public class HotPlace extends TimeBaseEntity {
+
+    @Id @GeneratedValue
+    @Column(name = "hot_place_id")
+    private Long id;
+    @Lob
+    @Column(nullable = false)
+    private String content;
+    @Embedded
+    private Place place;
+    private int hitCount;
+    private int likeCount;
+    private int scrapCount;
+    private int commentCount;
+    @Enumerated(STRING)
+    @Column(nullable = false, length = 20)
+    private Active active;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Builder
+    public HotPlace(Long id, String content, Place place, int hitCount, int likeCount, int scrapCount, int commentCount, Active active, Member member) {
+        this.id = id;
+        this.content = content;
+        this.place = place;
+        this.hitCount = hitCount;
+        this.likeCount = likeCount;
+        this.scrapCount = scrapCount;
+        this.commentCount = commentCount;
+        this.active = active;
+        this.member = member;
+    }
+}
