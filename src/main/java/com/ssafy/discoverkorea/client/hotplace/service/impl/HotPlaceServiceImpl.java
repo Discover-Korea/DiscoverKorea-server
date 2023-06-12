2cf978da3496a0ec1cc081b4515e8d1caef01090
chaos0103 <lyt1228@naver.com>
2023-06-12 17:37:34 +0900

    feat(hotPlace): DK-23 핫플레이스 등록 서비스 구현

--- src/main/java/com/ssafy/discoverkorea/client/hotplace/service/impl/HotPlaceServiceImpl.java
+++ src/main/java/com/ssafy/discoverkorea/client/hotplace/service/impl/HotPlaceServiceImpl.java
@@ -1,11 +1,16 @@
 package com.ssafy.discoverkorea.client.hotplace.service.impl;
 
+import com.ssafy.discoverkorea.client.hotplace.HotPlace;
+import com.ssafy.discoverkorea.client.hotplace.Place;
 import com.ssafy.discoverkorea.client.hotplace.repository.HotPlaceRepository;
 import com.ssafy.discoverkorea.client.hotplace.service.HotPlaceService;
 import com.ssafy.discoverkorea.client.hotplace.service.dto.AddHotPlaceDto;
+import com.ssafy.discoverkorea.client.member.Member;
 import com.ssafy.discoverkorea.client.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +21,18 @@ public class HotPlaceServiceImpl implements HotPlaceService {
 
     @Override
     public Long addHotPlace(String loginId, AddHotPlaceDto dto) {
-        return null;
+        Member member = memberRepository.findByLoginId(loginId)
+                .orElseThrow(NoSuchElementException::new);
+
+        Place place = Place.builder()
+                .placeName(dto.getPlaceName())
+                .roadAddress(dto.getRoadAddress())
+                .longitude(dto.getLongitude())
+                .latitude(dto.getLatitude())
+                .build();
+        HotPlace hotPlace = HotPlace.createHotPlace(member.getId(), dto.getContent(), place, dto.getFiles());
+
+        HotPlace savedHotPlace = hotPlaceRepository.save(hotPlace);
+        return savedHotPlace.getId();
     }
 }
