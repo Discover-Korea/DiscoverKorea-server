cb674a3aef1147ac01d4a829550dfc2765201a8c
chaos0103 <lyt1228@naver.com>
2023-06-12 17:31:36 +0900

    feat(hotPlace): DK-23 핫플레이스 연관관계 편의 메서드 구현

--- src/main/java/com/ssafy/discoverkorea/client/hotplace/HotPlace.java
+++ src/main/java/com/ssafy/discoverkorea/client/hotplace/HotPlace.java
@@ -3,6 +3,7 @@ package com.ssafy.discoverkorea.client.hotplace;
 import com.ssafy.discoverkorea.client.member.Member;
 import com.ssafy.discoverkorea.common.entity.Active;
 import com.ssafy.discoverkorea.common.entity.TimeBaseEntity;
+import com.ssafy.discoverkorea.common.entity.UploadFile;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +13,7 @@ import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.ssafy.discoverkorea.common.entity.Active.*;
 import static javax.persistence.EnumType.*;
 import static javax.persistence.FetchType.*;
 import static lombok.AccessLevel.PROTECTED;
@@ -44,6 +46,7 @@ public class HotPlace extends TimeBaseEntity {
     @OneToMany(mappedBy = "hotPlace", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<HotPlaceImage> images = new ArrayList<>();
 
+    //== 생성자 ==//
     @Builder
     public HotPlace(Long id, String content, Place place, int hitCount, int likeCount, int scrapCount, int commentCount, Active active, Member member, List<HotPlaceImage> images) {
         this.id = id;
@@ -55,6 +58,36 @@ public class HotPlace extends TimeBaseEntity {
         this.commentCount = commentCount;
         this.active = active;
         this.member = member;
+        this.images = images;
+    }
+
+    //== 연관관계 편의 메서드==//
+    public static HotPlace createHotPlace(Long memberId, String content, Place place, List<UploadFile> files) {
+        HotPlace hotPlace = HotPlace.builder()
+                .content(content)
+                .place(place)
+                .hitCount(0)
+                .likeCount(0)
+                .scrapCount(0)
+                .commentCount(0)
+                .active(ACTIVE)
+                .member(Member.builder().id(memberId).build())
+                .build();
+
+        List<HotPlaceImage> images = new ArrayList<>();
+        for (UploadFile file : files) {
+            HotPlaceImage image = HotPlaceImage.builder()
+                    .file(file)
+                    .hotPlace(hotPlace)
+                    .build();
+            images.add(image);
+        }
+
+        hotPlace.insertImages(images);
+        return hotPlace;
+    }
+
+    private void insertImages(List<HotPlaceImage> images) {
         this.images = images;
     }
 }
