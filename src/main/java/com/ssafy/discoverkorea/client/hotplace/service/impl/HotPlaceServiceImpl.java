ee3d7cf9a9703af63572939cdec63bbaa09e989a
chaos0103 <lyt1228@naver.com>
2023-06-13 23:17:34 +0900

    feat(hotPlace): DK-30 핫플레이스 스크랩 취소 서비스 구현

--- src/main/java/com/ssafy/discoverkorea/client/hotplace/service/impl/HotPlaceServiceImpl.java
+++ src/main/java/com/ssafy/discoverkorea/client/hotplace/service/impl/HotPlaceServiceImpl.java
@@ -136,6 +136,14 @@ public class HotPlaceServiceImpl implements HotPlaceService {
 
     @Override
     public Long cancelScrap(String loginId, Long hotPlaceId) {
-        return null;
+        Long hotPlaceScrapId = hotPlaceScrapRepository.findByLoginIdAndHotPlaceId(loginId, hotPlaceId)
+                .orElseThrow(NoSuchElementException::new);
+
+        HotPlace hotPlace = hotPlaceRepository.findById(hotPlaceId)
+                .orElseThrow(NoSuchElementException::new);
+        hotPlace.decreaseScrapCount();
+
+        hotPlaceScrapRepository.deleteById(hotPlaceScrapId);
+        return hotPlaceScrapId;
     }
 }
