90aba429c531cf323c920f6c710eacd422a36d8b
chaos0103 <lyt1228@naver.com>
2023-06-14 18:22:51 +0900

    feat(admin): DK-34 관리자 등록 서비스 구현

--- src/main/java/com/ssafy/discoverkorea/admin/admin/service/impl/AdminServiceImpl.java
+++ src/main/java/com/ssafy/discoverkorea/admin/admin/service/impl/AdminServiceImpl.java
@@ -1,10 +1,17 @@
 package com.ssafy.discoverkorea.admin.admin.service.impl;
 
+import com.ssafy.discoverkorea.admin.admin.Admin;
 import com.ssafy.discoverkorea.admin.admin.repository.AdminRepository;
 import com.ssafy.discoverkorea.admin.admin.service.AdminService;
 import com.ssafy.discoverkorea.admin.admin.service.dto.RegisterAdminDto;
+import com.ssafy.discoverkorea.common.exception.DuplicateException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.Optional;
+
+import static com.ssafy.discoverkorea.common.entity.Active.ACTIVE;
 
 @Service
 @RequiredArgsConstructor
@@ -14,6 +21,31 @@ public class AdminServiceImpl implements AdminService {
 
     @Override
     public Long register(RegisterAdminDto dto) {
-        return null;
+        Optional<Long> existLoginId = adminRepository.existLoginId(dto.getLoginId());
+        if (existLoginId.isPresent()) {
+            throw new DuplicateException();
+        }
+
+        Optional<Long> existTel = adminRepository.existTel(dto.getTel());
+        if (existTel.isPresent()) {
+            throw new DuplicateException();
+        }
+
+        Optional<Long> existEmail = adminRepository.existEmail(dto.getEmail());
+        if (existEmail.isPresent()) {
+            throw new DuplicateException();
+        }
+
+        Admin admin = Admin.builder()
+                .loginId(dto.getLoginId())
+                .loginPw(dto.getLoginPw())
+                .name(dto.getName())
+                .tel(dto.getTel())
+                .email(dto.getEmail())
+                .active(ACTIVE)
+                .roles(Collections.singletonList("ADMIN"))
+                .build();
+        Admin savedAdmin = adminRepository.save(admin);
+        return savedAdmin.getId();
     }
 }
