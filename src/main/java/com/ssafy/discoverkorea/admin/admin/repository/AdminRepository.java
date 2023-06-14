33e327286c3c0138a5207fe52a4127bf3435fcdc
chaos0103 <lyt1228@naver.com>
2023-06-14 17:41:22 +0900

    feat(admin): DK-34 AdminRepository 생성 및 DataJPA 적용

---
+++ src/main/java/com/ssafy/discoverkorea/admin/admin/repository/AdminRepository.java
@@ -0,0 +1,7 @@
+package com.ssafy.discoverkorea.admin.admin.repository;
+
+import com.ssafy.discoverkorea.admin.admin.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+}
