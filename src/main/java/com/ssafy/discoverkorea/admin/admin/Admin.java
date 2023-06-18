891833e9087f300ca814dc533d898f0cdc6ed404
chaos0103 <lyt1228@naver.com>
2023-06-18 23:41:55 +0900

    feat(admin): DK-36 관리자 삭제 비즈니스 로직 골격 생성

--- src/main/java/com/ssafy/discoverkorea/admin/admin/Admin.java
+++ src/main/java/com/ssafy/discoverkorea/admin/admin/Admin.java
@@ -78,6 +78,10 @@ public class Admin extends TimeBaseEntity implements UserDetails {
         this.email = newEmail;
     }
 
+    public void remove() {
+
+    }
+
     //== 시큐리티 설정 ==//
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
